a50af2e79dbb9a50677c0a964971aa415e084549
abhisheku <abhishekuofficialid@gmail.com>
2025-02-08 13:51:01 +0530

    Add lines

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from fpdf import FPDF
 import pandas as pd
 
-pdf=FPDF(orientation='P',unit='mm', format='A4')
+pdf=FPDF(orientation='P',unit='mm',format='A4')
 pdf.set_auto_page_break(auto=False,margin=0)
 
 df=pd.read_csv('topics.csv')
 
+
 for index,row in df.iterrows():
     pdf.add_page()
-    # Set Header
-    pdf.set_font(family='Times', style='B', size=24)
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row['Topic'], align='L',
-             ln=1)
-    pdf.line(10, 21, 200, 21)
+    # Set header
+    pdf.set_font(family='Times',style='B',size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0,h=12,txt=row['Topic'],align='L',ln=1)
 
-    # Set Footer
+    # Set lines
+    for y in range(21,290,10):
+        pdf.line(10,y,200,y)
+
+    # Set footer
     pdf.ln(260)
     pdf.set_font(family='Times', style='I', size=10)
     pdf.set_text_color(180, 180, 180)
@@ -23,17 +26,14 @@ for index,row in df.iterrows():
 
     for i in range(row['Pages']-1):
         pdf.add_page()
-        # Set Footer
-        pdf.ln(270)
+        # Set lines
+        for y in range(21, 290, 10):
+            pdf.line(10, y, 200, y)
+        # Set footer
+        pdf.ln(272)
         pdf.set_font(family='Times', style='I', size=10)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row['Topic'], align='R')
-
-
-
-
-
-
 
 
 
